a750c28af0858536b78c0ee2e02ed4898c482fcc
lavanya-thangamani <lavanyathangamani12@gmail.com>
2023-05-07 19:53:15 +0530

    initial commit

---
+++ NotesApp/app/src/main/java/com/example/notesapp/AddEditNoteActivity.kt
@@ -0,0 +1,74 @@
+package com.example.notesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddEditNoteActivity : AppCompatActivity() {
+
+    lateinit var noteTitleEdt: EditText
+    lateinit var noteEdt: EditText
+    lateinit var saveBtn: Button
+
+    lateinit var viewModel: NoteViewModel
+    var noteID = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_note)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NoteViewModel::class.java)
+
+        noteTitleEdt = findViewById(R.id.idEdtNoteName)
+        noteEdt = findViewById(R.id.idEdtNoteDesc)
+        saveBtn = findViewById(R.id.idBtn)
+
+        val noteType = intent.getStringExtra("noteType")
+        if (noteType.equals("Edit")) {
+            val noteTitle = intent.getStringExtra("noteTitle")
+            val noteDescription = intent.getStringExtra("noteDescription")
+            noteID = intent.getIntExtra("noteId", 0)
+            saveBtn.setText("Update Note")
+            noteTitleEdt.setText(noteTitle)
+            noteEdt.setText(noteDescription)
+        } else {
+            saveBtn.setText("Save Note")
+        }
+
+        saveBtn.setOnClickListener {
+            val noteTitle = noteTitleEdt.text.toString()
+            val noteDescription = noteEdt.text.toString()
+
+            if (noteType.equals("Edit")) {
+                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                    val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
+                    val currentDateAndTime: String = sdf.format(Date())
+                    val updatedNote = Note(noteTitle, noteDescription, currentDateAndTime)
+                    updatedNote.id = noteID
+                    viewModel.updateNote(updatedNote)
+                    Toast.makeText(this, "Note Updated..", Toast.LENGTH_LONG).show()
+                }
+            }
+            else {
+                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                    val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
+                    val currentDateAndTime: String = sdf.format(Date())
+
+                    viewModel.addNote(Note(noteTitle, noteDescription, currentDateAndTime))
+                    Toast.makeText(this, "$noteTitle Added", Toast.LENGTH_LONG).show()
+                }
+            }
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            this.finish()
+        }
+    }
+}
---
+++ NotesApp/app/src/main/java/com/example/notesapp/NotesDao.kt
@@ -0,0 +1,19 @@
+package com.example.notesapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by timeStamp DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
